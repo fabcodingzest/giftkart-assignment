16fe3562876ea1bb43890abd01dcc27275fea5f3
fabcodingzest <fabrizvi786@gmail.com>
2022-09-28 03:36:49 +0530

    added searchbar to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Searchbar from "./components/Searchbar";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Searchbar />
+    </div>
+  );
 }
 
 export default App;
