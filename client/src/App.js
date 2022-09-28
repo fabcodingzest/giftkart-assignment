b6bef7f41c6a490c9ad59f479fddeab9ad0382eb
fabcodingzest <fabrizvi786@gmail.com>
2022-09-28 17:59:36 +0530

    added products on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Carousel from "./components/Carousel";
+import ProductList from "./components/ProductList";
 import Searchbar from "./components/Searchbar";
+import { baseAPI, carouselImages } from "./utilities/constants";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      fetch(`${baseAPI}/products`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setProducts(data);
+        })
+        .catch((error) => alert(error.message));
+    };
+    getProducts();
+  }, []);
+
   return (
-    <div>
+    <div className="app">
       <Searchbar />
+      <Carousel slides={carouselImages} fullScreen />
+      <ProductList data={products} />
     </div>
   );
 }
