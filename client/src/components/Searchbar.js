6cc11fc35d150b02704fd0f27ca30391feff508b
fabcodingzest <fabrizvi786@gmail.com>
2022-09-28 07:57:06 +0530

    added: search autocomplete functionality and styles

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -1,27 +1,79 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { debounce } from "../utilities/debounce";
 import { ReactComponent as SearchIcon } from "../assets/search.svg";
+import { baseAPI } from "../utilities/constants";
+
+const SearchItem = ({ img, text }) => {
+  return (
+    <div className="search__result">
+      {img && (
+        <div className="search__result--img">
+          <img src={img} alt={text} />
+        </div>
+      )}
+      <p>{text}</p>
+    </div>
+  );
+};
 
 function Searchbar() {
-  const handleSearch = (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
+  const [results, setResults] = useState([]);
+  const [dropDownOpen, setDropDownOpen] = useState(false);
+
+  const fetchData = (query) => {
+    fetch(`${baseAPI}/products/${encodeURI(query)}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setDropDownOpen(true);
+        setResults(json);
+      })
+      .catch((err) => {
+        alert("Something went wrong. Please try again later!");
+      });
   };
+  const handleSearch = (e) => {
+    const { value } = e.target;
+    if (value !== "") {
+      fetchData(value);
+    }
+    setDropDownOpen(false);
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSearch = useCallback(debounce(handleSearch, 500), []);
+
   return (
-    <div className="container search">
+    <div className="search">
       <div id="searchbox">
-        {/* create input */}
-        {/* add debouncing for input */}
-        {/* create dropdown with static data and handle no result and result state */}
-        {/* Create the dropdown items as links to product page */}
         <input
           id="search-input"
           type="text"
-          onChange={debounce(handleSearch, 1000)}
+          onChange={debouncedSearch}
+          onFocus={(e) => {
+            setDropDownOpen(true);
+            debouncedSearch();
+          }}
+          onBlur={() => setDropDownOpen(false)}
+          autoComplete="off"
         />
         <button id="search-btn" onClick={() => console.log("hello btn")}>
           <SearchIcon id="search-icon" />
         </button>
+        {dropDownOpen && (
+          <div className="dropdown">
+            {results.length > 0 ? (
+              results.map((item) => (
+                <SearchItem
+                  img={item.listOfImages[0]}
+                  text={item.productName}
+                  key={item.id}
+                />
+              ))
+            ) : (
+              <SearchItem text="No Items Found" />
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
