fbd9f19f2bbc298165c8df96f2f9145dceddc06a
fabcodingzest <fabrizvi786@gmail.com>
2022-09-30 04:59:05 +0530

    added: debouncing

--- client/src/components/Arrow.js
+++ client/src/components/Arrow.js
@@ -1,11 +1,12 @@
 import { ReactComponent as RightArrow } from "../assets/right-arrow.svg";
 import { ReactComponent as LeftArrow } from "../assets/left-arrow.svg";
+import { debounce } from "../utilities/debounce";
 
 function Arrow({ direction, handleClick }) {
   return (
     <button
       className="arrow"
-      onClick={handleClick}
+      onClick={debounce(handleClick, 300)}
       style={direction === "right" ? { right: 0 } : { left: 0 }}>
       {direction === "right" ? <RightArrow /> : <LeftArrow />}
     </button>
